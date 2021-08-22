5818f991e2286e6ce9b490df452155e0564a223e
FrAzenV2 <54308705+FrAzenV2@users.noreply.github.com>
2021-08-22 18:03:30 +0300

    Add bot config generation feature

---
+++ Assets/Bots Configs/ScriptableObjectConfig/BotConfig.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using СlothesConfigs.ScriptableObjectConfig;
+
+namespace Bots_Configs.ScriptableObjectConfig
+{
+    [CreateAssetMenu(fileName = "New Bot Config", menuName = "Bots/new Bot Config", order = 0)]
+    public class BotConfig : ScriptableObject
+    {
+        [SerializeField] private string _botName;
+        [SerializeField] private ClothesConfig[] _clothes;
+        [SerializeField] private Sprite _baseSprite;
+        [SerializeField] private int _botId;
+
+        public int BotId
+        {
+            get => _botId;
+            set => _botId = value;
+        }
+
+        public void SetName(string botName)
+        {
+            _botName = botName;
+        }
+
+        public void SetBaseSprite(Sprite sprite)
+        {
+            _baseSprite = sprite;
+        }
+        
+        public void SetClothes(ClothesConfig[] clothesConfigs)
+        {
+            _clothes = clothesConfigs;
+        }
+    }
+}
