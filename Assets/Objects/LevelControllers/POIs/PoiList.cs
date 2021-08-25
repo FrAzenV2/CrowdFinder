39c691471ae17733a0a8d71aad5250333a446e45
FrAzenV2 <54308705+FrAzenV2@users.noreply.github.com>
2021-08-25 19:46:46 +0300

    Add player POI transition

---
+++ Assets/Objects/LevelControllers/POIs/PoiList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Objects.LevelControllers.Scripts
+{
+    public class PoiList : MonoBehaviour
+    {
+        public List<POI> Pois;
+
+        private void OnEnable()
+        {
+            foreach (var poi in Pois)
+            {
+                poi.PlayerSteppedIn += ResetAllCameraPriorities;
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var poi in Pois)
+            {
+                poi.PlayerSteppedIn -= ResetAllCameraPriorities;
+            }
+        }
+
+        private void ResetAllCameraPriorities()
+        {
+            foreach (var poi in Pois)
+            {
+                poi.ResetCameraPriority();
+            }
+        }
+    }
+}
