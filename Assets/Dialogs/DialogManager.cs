c42c82230d425e9f17fb4286fa112660d1e77997
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2021-08-28 16:15:17 +0300

    Clamp dialogs to screen

--- Assets/Dialogs/DialogManager.cs
+++ Assets/Dialogs/DialogManager.cs
@@ -26,6 +26,7 @@ namespace Managers
             var canvasPosition = Camera.main.WorldToScreenPoint(dialog.fromBot.dialogPoint.transform.position);
             DialogBox dialogBox = Instantiate(_dialogBoxPrefab, canvasPosition , Quaternion.identity, _dialogParent);
             dialogBox.Initialize(dialog);
+            ClampToScreen(_dialogParent.GetComponent<RectTransform>(), dialogBox.GetComponent<RectTransform>());
             _currentDialog = dialogBox;
         }
 
@@ -41,6 +42,16 @@ namespace Managers
             dialog.fromBot = trait.Sender;
             dialog.text = trait.GetTraitText();
             return dialog;
+        }
+
+        private void ClampToScreen(RectTransform canvas, RectTransform obj)
+        {
+            var sizeDelta = canvas.sizeDelta - obj.sizeDelta;
+            var objPivot = obj.pivot;
+            var position = obj.anchoredPosition;
+            position.x = Mathf.Clamp(position.x, -sizeDelta.x * objPivot.x, sizeDelta.x * (1 - objPivot.x));
+            position.y = Mathf.Clamp(position.y, -sizeDelta.y * objPivot.y, sizeDelta.y * (1 - objPivot.y));
+            obj.anchoredPosition = position;
         }
 
         private DialogBox _currentDialog;
